4fdfc76b695a3f1ed50326487492f3d6e8223b61
Sebastian Krzyszkowiak <dos@dosowisko.net>
2018-10-08 19:57:54 +0200

    Use libswresample instead of libavresample, which is deprecated in FFmpeg

--- src/config.cmake.hh
+++ src/config.cmake.hh
@@ -14,7 +14,7 @@
 // FFMPEG libraries use changing include file names... Get them from CMake.
 #define AVCODEC_INCLUDE <@AVCodec_INCLUDE@>
 #define AVFORMAT_INCLUDE <@AVFormat_INCLUDE@>
-#define AVRESAMPLE_INCLUDE <@AVResample_INCLUDE@>
+#define SWRESAMPLE_INCLUDE <@SWResample_INCLUDE@>
 #define SWSCALE_INCLUDE <@SWScale_INCLUDE@>
 #define AVUTIL_INCLUDE <@AVUtil_INCLUDE@>
 #define AVUTIL_OPT_INCLUDE <libavutil/opt.h> //HACK to get AVOption class!
